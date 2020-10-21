32fb83d9cb73afde0f8b76a0800f8092abf7fce8
tiwaryabhinav <50542527+tiwaryabhinav@users.noreply.github.com>
2020-10-21 16:59:40 +0530

    Add files via upload

---
+++ public/css/js/logic.js
@@ -0,0 +1,32 @@
+const form=document.querySelector('form');
+const input=document.querySelector('input');
+const p1=document.getElementById('msg1');
+const p2=document.getElementById('msg2');
+p1.textContent='';
+p2.textContent='';
+form.addEventListener('submit',(e)=>
+{
+    e.preventDefault();
+    const val=input.value;
+    p1.textContent='Loading';
+    p2.textContent='';
+    const url='http://localhost:3000/products?search='+val;
+    fetch(url).then((res)=>{
+        res.json().then((data)=>{
+            if(data.error)
+            {
+                p1.textContent=data.error;
+                p2.textContent='';
+            }
+            else
+            {
+                p1.textContent='Location is '+data.location;
+                //console.log(data.location);
+                //console.log(data.information);
+                p2.textContent='Temparature at '+data.location+' is '+data.information.temperature+' degree celcius and pressure is '+data.information.pressure+" millibar";
+                //console.log(data.information.temperature);
+                //console.log(data.information.pressure);
+            }
+        })
+    })
+})
